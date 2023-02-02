2f2bb16026d98aa1caef4ea2611f8ccec0c0aaa5
dlewisoconnell <112989604+dlewisoconnell@users.noreply.github.com>
2023-02-02 17:58:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var addToDontButton = document.getElementById('addToDont');
+var addTotalShame = document.getElementById('totalShame');
+let toDontContainer = document.getElementById('toDontContainer');
+let inputField = document.getElementById('inputField');
+let shameArray = [];
+
+addToDontButton.addEventListener('click', function(){
+  var paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputField.value;
+  toDontContainer.appendChild(paragraph);
+  inputField.value = ""; 
+  shameArray.push(1);
+  paragraph.addEventListener('dblclick', function(){
+    toDontContainer.removeChild(paragraph);
+    shameArray.pop(1);
+      })
+    })
+    
+//addTotalShame.addEventListener('click', function(){
+//   if (shameArray.length === 0) {window.alert("Great Job!")};
+//  else {window.alert("Shame!")}})
+
+addTotalShame.addEventListener('click', function(){
+   if (shameArray.length === 0) {window.alert("Great Job!")}
+  else {window.alert("Shame!");}})
