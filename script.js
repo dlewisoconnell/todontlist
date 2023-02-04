7e5985b6dfa3218147ab76920f748617cfb01588
dlewisoconnell <112989604+dlewisoconnell@users.noreply.github.com>
2023-02-04 15:18:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ let inputField = document.getElementById('inputField');
 let shameArray = [];
 
 addToDontButton.addEventListener('click', function(){
+
   var paragraph = document.createElement('p');
   paragraph.classList.add('paragraph-styling');
   paragraph.innerText = inputField.value;
   toDontContainer.appendChild(paragraph);
-  inputField.value = ""; 
-  shameArray.push(1);
+  if (inputField.value === "") alert("Error: input field empty");
+  else shameArray.push(1);
+  inputField.value = "Another?";
   paragraph.addEventListener('dblclick', function(){
   toDontContainer.removeChild(paragraph);
   shameArray.pop(1);
       })
     })
+
 
 addTotalShame.addEventListener('click', function(){
    if (shameArray.length === 0) {window.alert("Great Job!")}
